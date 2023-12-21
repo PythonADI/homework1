8da030e3cda96d992f056afcd73990e76fa91f93
Luka Tavkhelidze <52665503+tavkhelidzeluka@users.noreply.github.com>
2023-12-21 15:02:49 +0400

    Fix test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,15 +8,23 @@ from homework1.main import main
 class TestMainFunction(TestCase):
     def test_main(self):
         base_dir = Path(__file__).resolve().parent.parent
-        read_me = base_dir / 'README.md'
         with io.StringIO() as buf:
             with contextlib.redirect_stdout(buf):
                 main()
 
             exercises = [
-                exercise[3:].strip()
-                for exercise in
-                read_me.read_bytes().decode().split('\n')[6:-1]
+                'Hello World!',
+                5 + 7,
+                37 - 8,
+                -90 * 4,
+                100 / 3,
+                0.2 + 0.1,
+                7.068 * 4.3,
+                8 ** 2,
+                9 ** 3,
+                5 / 3,
+                5 // 3,
+                99 % 7
             ]
 
             output = buf.getvalue().split('\n')[:-1]
@@ -24,9 +32,8 @@ class TestMainFunction(TestCase):
 
             for i, (done_exercise, exercise) in enumerate(zip(output, exercises), start=1):
                 msg = f'Wrong answer for exercise {i}: {exercise}, you have {done_exercise}'
-                print(exercise, done_exercise)
                 self.assertEqual(
                     done_exercise,
-                    str(eval(exercise)),
+                    str(exercise),
                     msg
                 )
